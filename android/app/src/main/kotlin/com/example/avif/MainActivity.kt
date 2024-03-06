fd485368575de40e7fd2f804ad5ef160d3290118
Rafael-Lasari-Aschidamini <rafaelaschidamini@gmail.com>
2024-03-06 14:16:25 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/avif/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_avif_macos
+import flutter_image_compress_macos
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAvifPlugin.register(with: registry.registrar(forPlugin: "FlutterAvifPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
